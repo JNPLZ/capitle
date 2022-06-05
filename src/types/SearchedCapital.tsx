b3803e6d37656395701c1f26f307cf97e5a98935
THMBLN <jennifer.pelz@gmail.com>
2022-06-05 19:57:04 +0200

    Changed calculation of random capital.

--- src/types/SearchedCapital.tsx
+++ src/types/SearchedCapital.tsx
@@ -1,3 +1,4 @@
+import seedrandom from 'seedrandom';
 import Capital from './Capital';
 import AllCapitals from './AllCapitals';
 
@@ -5,13 +6,11 @@ class SearchedCapital {
   public static capital: Capital;
 
   public static findSearchedCapitalOfTheDay() {
-    const today = new Date().toLocaleDateString('de-DE');
-    const [date, month, year] = today.split('.');
-    const dayCode = Date.UTC(parseInt(year, 10), parseInt(month, 10) - 1, parseInt(date, 10));
-    const SHUFFLE_KEY = process.env.REACT_APP_SHUFFLE_KEY || '1';
-    const key = Math.floor(dayCode / parseInt(`${SHUFFLE_KEY}5`, 10)) % AllCapitals.getCapitals().length;
-    this.capital = AllCapitals.getCapitals()[key];
-    // console.log(this.capital);
+    const todayString = new Date().toLocaleDateString('de-DE');
+    const capitalIndex = Math.floor(
+      seedrandom.alea(todayString)() * AllCapitals.getCapitals().length,
+    );
+    this.capital = AllCapitals.getCapitals()[capitalIndex];
   }
 
   public static getSearchedCapitalOfTheDay() {
